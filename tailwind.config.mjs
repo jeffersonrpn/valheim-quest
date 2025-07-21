ed6ca49a02a5b6fe2abd7f5cd25d07a3ede17536
Jefferson Neves <jefferson.rpn@gmail.com>
2025-07-21 19:47:24 -0300

    Create hero card with aspect-ratio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,20 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Macondo", ...defaultTheme.fontFamily.sans],
         serif: ["Roboto Slab", ...defaultTheme.fontFamily.serif],
       },
+      backgroundImage: {
+        "hero-card": "url('/imgs/bg.png')",
+      },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
